5ebfe0fe601dd7e4bdd463e26e1f28ab933ea3fc
George MacKerron <george@mackerron.co.uk>
2019-03-28 14:51:21 +0000

    Tabs -> spaces

--- double-six.rb
+++ double-six.rb
@@ -6,15 +6,15 @@ $stdout.puts $stdin.gets
 # now repeat until end of file
 until $stdin.eof?
 
-	# read 6 lines
-	inlines = (0..5).map { $stdin.gets }
+  # read 6 lines
+  inlines = (0..5).map { $stdin.gets }
 
-	# spit them out, first in the original order,
-	# and then again after switching adjacent pairs
-	[0, 1, 
-	 2, 3, 
-	 4, 5, 
-	 1, 0, 
-	 3, 2, 
-	 5, 4].each { |index| $stdout.puts(inlines[index] || '—') }
+  # spit them out, first in the original order,
+  # and then again after switching adjacent pairs
+  [0, 1, 
+   2, 3, 
+   4, 5, 
+   1, 0, 
+   3, 2, 
+   5, 4].each { |index| $stdout.puts(inlines[index] || '—') }
 end
